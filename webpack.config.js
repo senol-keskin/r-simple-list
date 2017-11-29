98a3c8bbb6d96c83af18f78201c6f09695215557
senol <msenolkeskin@gmail.com>
2017-11-29 23:19:46 +0300

    fixed build error

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,44 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 function getDevTool() {
-    if (process.env.NODE_ENV !== 'production') {
-        return 'source-map'; //enables source map
-    }
-    
-    return false; 
+  if (process.env.NODE_ENV !== 'production') {
+    return 'source-map'; //enables source map
+  }
+
+  return false;
 }
 
 module.exports = {
-    entry: {
-        main: './src/scripts/main.js'
-    },
-    output: {
-        filename: 'dist/scripts/[name].js'
-    },
-    devtool: getDevTool(),
-    devServer: {
-         
-        port: 3000
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('css-loader!sass-loader')
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin({
-            filename : 'dist/styles/main.css',
-            allChunks: true
-        })
-    ]
+  entry: {
+    main: './src/scripts/main.js',
+  },
+  output: {
+    filename: 'dist/scripts/[name].js',
+  },
+  devtool: getDevTool(),
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'dist/styles/main.css',
+      allChunks: true,
+    }),
+  ],
 };
